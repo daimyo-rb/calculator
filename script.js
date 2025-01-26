912e79c6353b1754138d51df300fc589ce79132e
daimyo-rb <daimyo@roshibros.com>
2025-01-26 15:26:12 -0600

    Add calculator UI in html and css

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log('loaded');
+function add(num1, num2){
+    return num1 + num2;
+}
+function subtract(num1, num2){
+    return num1 - num2;
+}
+function multiply(num1, num2){
+    return num1 * num2;
+}
+function divide(num1, num2){
+    return num1 / num2;
+}
+function operate(num1, num2, operator){
+    if (operator === '+') return add(num1, num2);
+    if (operator === '-') return subtract(num1, num2);
+    if (operator === '*') return multiply(num1, num2);
+    if (operator === '/') return divide(num1, num2);
+}
+function updateDisplay(arg){
+    let display = document.querySelector('.display');
+    display.innerText = arg;
+}
+function clearDisplay(){
+    updateDisplay('0');
+}
+
+let arg1;
+let arg2;
+let operator;
+
+function buttonHandler(e){
+    console.log(e); 
+}
+let buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', buttonHandler);
+})
